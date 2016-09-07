3523f2df73e2082628f3c45d749122efee9db433
samohval <m.scomoroh@gmail.com>
2016-09-07 13:12:11 +0300

    delete test_conn_file

--- test_connection.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# author: maxim.samohval@protonmail.com
-# date: 07/09/2016
-import config
-
-pars = config.GetSettings().get_all_parameters()
-print pars
