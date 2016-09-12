2e24c4f3222bc4cd467d6d83ddff325469805ef5
samohval.m <maxim.samohval@protonmail.com>
2016-09-12 08:50:08 +0300

    Завершил кейс по по ожиданию сохранения модели

--- test_bonus_model.py
+++ test_bonus_model.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# author: maxim.samohval@protonmail.com
 # date: 08/09/2016
 
 __version__ = "0.1"
 __author__ = "Samohval Maxim  <maxim.samohval@protonmail.com>"
 
 
-import unittest,datetime,config
+import unittest,datetime,config,time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -20,9 +20,7 @@ Init_connection = config.GetSettings().get_all_parameters()
 TEST_MODEL_NAME = 'TEST_MODEL '+str(datetime.datetime.now())
 now_date = datetime.date.today()
 cur_day = now_date.day
-delta = datetime.timedelta(days=7)
-end_day = now_date + delta
-end_day = end_day.day
+
 
 #authorization
 srv = Init_connection.get('server')
@@ -43,37 +41,40 @@ class Test_Loyality_bonus_models(unittest.TestCase):
         self.verificationErrors = []
 
     def LoginPass(self):
-            username = self.browser.find_element_by_name("username")
-            username.clear()
-            username.send_keys(user)
-            password = self.browser.find_element_by_name("password")
-            password.clear()
-            password.send_keys(pswd)
-            password.submit()
+        username = self.browser.find_element_by_name("username")
+        username.clear()
+        username.send_keys(user)
+        password = self.browser.find_element_by_name("password")
+        password.clear()
+        password.send_keys(pswd)
+        password.submit()
 
     def Authorize(self):
-                self.browser.get(srv)
-                self.LoginPass()
+        self.browser.get(srv)
+        self.LoginPass()
 
-    @unittest.skip('not supported')
+    # @unittest.skip('not supported')
     def test_coupons_positive(self):
         self.Authorize()
-        self.browser.get(coupons)
-        SearchElement = self.browser.find_element_by_id('lists')
+        driver = self.browser
+        driver.get(coupons)
+        SearchElement = driver.find_element_by_id('lists')
         self.assertIsNotNone(SearchElement)
-        self.browser.close()
+        driver.close()
 
-    @unittest.skip('not supported')
+    # @unittest.skip('not supported')
     def test_open_model_positive(self):
         self.Authorize()
-        self.browser.get(bonus_model)
-        model_link = self.browser.find_element_by_xpath('//tr[1]/td[1]/a').get_attribute('href')
-        self.browser.get(model_link)
-        Element = self.browser.find_element_by_id('remove_restriction')
+        driver = self.browser
+        driver.get(bonus_model)
+        model_link = driver.find_element_by_xpath('//tr[1]/td[1]/a').get_attribute('href')
+        driver.get(model_link)
+        Element = driver.find_element_by_id('remove_restriction')
         self.assertIsNotNone(Element)
         self.browser.close()
 
     # @unittest.skip('not supported')
+    # новая модель добавляется успешно
     def test_add_save_new_model_positive(self):
         self.Authorize()
         driver = self.browser
@@ -83,36 +84,34 @@ class Test_Loyality_bonus_models(unittest.TestCase):
         driver.find_element_by_id("first_start_day").click()
         driver.find_element_by_link_text(str(cur_day)).click()
         driver.find_element_by_id("first_end_day").click()
-        driver.find_element_by_link_text(str(end_day)).click()
+        driver.find_element_by_link_text(str(cur_day)).click()
         driver.find_element_by_link_text(u"Выбрать").click()
         driver.find_element_by_name("add-products-id").click()
         driver.find_element_by_xpath("(//input[@name='add-products-id'])[2]").click()
         driver.find_element_by_css_selector("#products-load-buttons > button.btn.btn-primary").click()
-        driver.find_element_by_id("save").click()
-        driver.save_screenshot('fire.png')
+        driver.find_element_by_id('save').click()
+        driver.save_screenshot('firefox_before_click.png')
+        wait = WebDriverWait(driver, 15)
+        element = wait.until(EC.invisibility_of_element_located((By.ID,'loading')))
+        driver.save_screenshot('firefox_after.click.png')
+        print driver.current_url
+        self.assertTrue("/edit/id/" in driver.current_url)
+        print driver.current_url
         driver.close()
 
-    @unittest.skip('not supported')
-    def test_modify_model_positive(self):
+    # @unittest.skip('not supported')
+    # проверить что изменённая модель отображается в списке моделей
+    def test_modified_model_visible_in_list_positive(self):
         self.Authorize()
         driver = self.browser
-        driver.find_element_by_id('model_name').clear()
-        driver.find_element_by_id('model_name').send_keys(TEST_MODEL_NAME)
-        driver.find_element_by_id("first_start_day").click()
-        driver.find_element_by_link_text(str(cur_day)).click()
-        driver.find_element_by_id("first_end_day").click()
-        driver.find_element_by_link_text(str(end_day)).click()
-        driver.find_element_by_link_text(u"Выбрать").click()
-        driver.find_element_by_name("add-products-id").click()
-        driver.find_element_by_xpath("(//input[@name='add-products-id'])[2]").click()
-        driver.find_element_by_css_selector("#products-load-buttons > button.btn.btn-primary").click()
-        driver.find_element_by_id("save").click()
+        driver.get(bonus_model)
+        self.assertTrue("/edit/id/" in driver.current_url)
         driver.close()
 
-        def tearDown(self):
-            self.assertEqual([],self.verificationErrors)
-            self.browser.close()
-            self.browser.quit()
+    def tearDown(self):
+        self.assertEqual([],self.verificationErrors)
+        self.browser.close()
+        self.browser.quit()
 
 
 
