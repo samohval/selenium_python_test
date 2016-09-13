765752d170313effa80f0ecb887d74bcaed57ad9
samohval.m <maxim.samohval@protonmail.com>
2016-09-13 16:11:13 +0300

    add new class

--- config.py
+++ config.py
@@ -6,38 +6,51 @@ __version__ = "0.1"
 __author__ = "Samohval Maxim  <maxim.samohval@protonmail.com>"
 
 import ConfigParser
+from selenium import webdriver
 
 _CONF_FILE = "/home/samohval/Dropbox/GIT/python_selenium_loyality/test.cfg"
 conf = ConfigParser.RawConfigParser()
 conf.read(_CONF_FILE)
 
+
+
 class GetSettings(object):
 
     def get_link(self,link_id="cards"):
-        try:
-            link = self.get_server() + conf.get("links",link_id)
-        except Exception as e:
-            raise
-        else:
-            pass
+        link = self.get_server() + conf.get("links",link_id)
         return link
     def get_end_of_link(self,link_id="empty"):
         link = conf.get("links",link_id)
         return link
-
     def get_server(self):
         return conf.get("connection","srv")
-
     def get_user(self):
         return conf.get("connection","user")
-
     def get_password(self):
             return conf.get("connection","pass")
-
     def get_all_parameters(self):
         params = dict(server = self.get_server(),user = self.get_user(),password = self.get_password())
         return params
 
+class MyBrowser(object):
+
+    def __init__(self):
+        self.base_url = GetSettings().get_all_parameters().get("server")
+        self.settings = GetSettings()
+
+    def main(self):
+        print "server base_url: -> " + self.base_url
+
+    def connect_auth_server(self,driver = None):
+        driver.get(self.base_url)
+        driver.find_element_by_name("username").clear()
+        driver.find_element_by_name("username").send_keys(self.settings.get_user())
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys(self.settings.get_password())
+        driver.find_element_by_css_selector("#login > #login").click()
+
+
+
 
 if __name__ == '__main__':
-    print 'main module'
+    MyBrowser().main()
